f66326f0d8d76d141d52452aeadcd928ca9e526f
Gordon Croft <gordon.croft@telus.net>
2026-08-12 14:16:48 -0700

    Array AT method

--- starter/script.js
+++ starter/script.js
@@ -74,6 +74,7 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+/*
 let arr = ['a', 'b', 'c', 'd', 'e'];
 
 // Slice
@@ -100,3 +101,16 @@ console.log(letters);
 
 //Join
 console.log(letters.join(' - '));
+*/
+////////////////////////////////////////////////
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+//Getting last element
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
+
+console.log('Jonas'.at(0));
+console.log('Jonas'.at(-1));
