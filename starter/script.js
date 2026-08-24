1ccc63cf6a5b250d1a1e94da4e1a826bdcc1f173
Gordon Croft <gordon.croft@telus.net>
2026-08-24 12:05:08 -0700

    Flat and FlatMap

--- starter/script.js
+++ starter/script.js
@@ -479,7 +479,6 @@ console.log(lastestLargeMovementIndex);
 console.log(
   `Your latest large movement was ${movements.length - lastestLargeMovementIndex} movements ago`,
 );
-*/
 
 // SOME
 console.log(movements);
@@ -499,3 +498,23 @@ const deposit = mov => mov > 0;
 console.log(movements.some(deposit));
 console.log(movements.every(deposit));
 console.log(movements.filter(deposit));
+*/
+
+const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+console.log(arr.flat());
+
+const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+console.log(arrDeep.flat(2));
+
+// Flat
+const overallBalance = accounts
+  .map(acc => acc.movements)
+  .flat()
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overallBalance);
+
+// FlatMap
+const overallBalance2 = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overallBalance2);
