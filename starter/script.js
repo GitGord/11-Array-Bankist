8f8d5fcafa835b931653e9b1952c9e1d26750b25
Gordon Croft <gordon.croft@telus.net>
2026-09-04 12:55:04 -0700

    Array Methods Practice

--- starter/script.js
+++ starter/script.js
@@ -694,7 +694,6 @@ labelBalance.addEventListener('click', function () {
   );
   console.log(movementsUI);
 });
-*/
 
 //////////////////////////////////////////////
 // Non-Destructive Alternatives
@@ -710,3 +709,59 @@ console.log(reversedMov);
 const newMovements = movements.with(1, 2000);
 console.log(newMovements);
 console.log(movements);
+*/
+//////////////////////////////////////////////////
+// Array Methods Practice
+
+//1.
+const bankDepositSum = accounts
+  .flatMap(acc => acc.movements)
+  .filter(mov => mov > 0)
+  .reduce((sum, cur) => sum + cur, 0);
+
+console.log(bankDepositSum);
+
+// 2.
+const numDeposits1000 = accounts
+  .flatMap(acc => acc.movements)
+  //   .filter(mov => mov >= 1000).length;
+  .reduce((count, cur) => (cur >= 1000 ? ++count : count), 0);
+console.log(numDeposits1000);
+
+// Prefex ++ operator
+let a = 10;
+console.log(a++);
+console.log(++a);
+console.log(a);
+
+// 3.
+// const sums = accounts
+const { deposits, withdrawals } = accounts
+  .flatMap(acc => acc.movements)
+  .reduce(
+    (sums, cur) => {
+      // cur > 0 ? (sums.deposits += cur) : (sums.withdrawals += cur);
+      sums[cur > 0 ? 'deposits' : 'withdrawals'] += cur;
+      return sums;
+    },
+    { deposits: 0, withdrawals: 0 },
+  );
+
+console.log(deposits, withdrawals);
+
+// 4.
+// this is a nice title -> This Is a Nice Title
+const convertTitleCase = function (title) {
+  const capitalize = str => str[0].toUpperCase() + str.slice(1);
+  const exceptions = ['a', 'an', 'and', 'the', 'but', 'or', 'on', 'with'];
+  const titleCase = title
+    .toLowerCase()
+    .split(' ')
+    .map(word => (exceptions.includes(word) ? word : capitalize(word)))
+    .join(' ');
+
+  return capitalize(titleCase);
+};
+console.log(convertTitleCase('this is a nice title'));
+console.log(convertTitleCase('this is a LONG title but not too long'));
+console.log(convertTitleCase('and here is another title with an EXAMPLE'));
